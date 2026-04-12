0568f0a236d9eeea2b5a642fdb4a71014b0947a9
Gustavohoe <gustavohoe74@gmail.com>
2026-04-12 16:12:00 -0300

    urls.py

--- app_solar_salesmen/urls.py
+++ app_solar_salesmen/urls.py
@@ -5,5 +5,8 @@ from. import views
 
 urlpatterns=[
     path('', views.login, name="login"),
-    path('cadastro/', views.cadastro, name="cadastro")
+    path('cadastro/', views.cadastro, name="cadastro"),
+    path('diretoria/', views.diretoria, name="diretoria"),
+    path('pagina-Inicial/', views.paginaInicial, name="paginaInicial"),
+    path('tornar-admin/', views.tornar_admin, name='tornar_admin')
 ]
