0db4f3e9d96e7d20d575a3094c0fab0528c2a2cc
David Mandel <dvdmndel@gmail.com>
2026-04-13 20:46:08 -0300

    Páginas de Vendedor e Diretor funcionais

--- app_solar_salesmen/views.py
+++ app_solar_salesmen/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.decorators import user_passes_test
+from django.http import JsonResponse
+from .models import Produto, Venda
+import json
 import sqlite3 as sql
 import pandas as pd
 import plotly.express as px
@@ -19,6 +23,10 @@ def login(request):
 
         if user:
             login_django(request, user)
+            if user.groups.filter(name='admin').exists():
+                return redirect('diretoria')  
+            else:
+                return redirect('paginaInicial')  
             return redirect('')
         else:
             messages.error(request, 'Username ou senha invalidos')
@@ -45,62 +53,125 @@ def cadastro(request):
 
         return redirect('login')
 
-def table_append(request):
-    if request.method == "POST":
-        id_produto = int(request.POST.get('selection'))
-        qtd = int(request.POST.get('qtd'))
-        connection = sql.connect('db.sqlite3')
-        cursor = connection.cursor()
-        cursor.execute("""
-        SELECT valor
-        FROM produtos 
-        WHERE id = %s
-        """, [id_produto])
-        valor = cursor.fetchone()[0]
-        fat = valor * qtd
-        id_vendedor = request.user.id
-        cursor.execute("""
-        INSERT INTO vendas (id_p, id_v, qtd, fat)
-        VALUES (%s, %s, %s, %s)
-        """, [id_produto, id_vendedor, qtd, fat])
-        connection.close()
-        return redirect('table_append')
+def is_admin(user):
+    return user.is_authenticated and user.groups.filter(name='admin').exists()
+
+@user_passes_test(is_admin, login_url='paginaInicial')
+def diretoria(request):
+    usuarios = User.objects.exclude(groups__name='admin')
+    usuariosf = User.objects.filter(groups__name='admin')
+    return render(request, 'diretoria.html', {
+        'usuarios': usuarios,
+        'usuariosf': usuariosf
+        })
+
+@user_passes_test(is_admin, login_url='paginaInicial')
+def tornar_admin(request):
+     if request.method == "POST":
+        user_id= request.POST.get('user_id')
+        user = get_object_or_404(User, id=user_id)
+        grupo, _ = Group.objects.get_or_create(name='admin')
+        acao = request.POST.get('acao')
+        if acao=="tornar":
+            user.groups.add(grupo)
+        else:
+            user.groups.remove(grupo)
+        return redirect('diretoria')
+     
+def paginaInicial(request):
+    produtos = Produto.objects.all()
+    vendas = Venda.objects.filter(vendedor=request.user)
+    return render(request, 'vendedor.html', {
+        'Produtos': produtos,  # tem que ser exatamente 'Produtos' para bater com o template
+        'vendas': vendas
+    })
+
+def diretoria(request):
+    from django.contrib.auth.models import User
+    usuarios = User.objects.filter(is_staff=False)
+    usuariosf = User.objects.filter(is_staff=True)
+    return render(request, 'diretoria.html', {
+        'usuarios': usuarios,
+        'usuariosf': usuariosf
+    })
 
 def makegraph(request):
-    connection = sql.connect('db.sqlite3')
-    cursor = connection.cursor()
     if request.method == "POST":
         graphtype = request.POST.get('graphtype')
+        data_inicio = request.POST.get('data_inicio')
+        data_fim = request.POST.get('data_fim')
+        vendas = Venda.objects.select_related('vendedor', 'produto').all()
+        if data_inicio:
+            vendas = Venda.filter(date__gte=data_inicio)
+        if data_fim:
+            vendas = Venda.filter(date__lte=data_fim)
+        rows = [(v.vendedor.username, v.produto.nome, v.qtd, v.fat) for v in vendas]
+        df = pd.DataFrame(rows, columns=['vendedor', 'produto', 'qtd', 'fat'])
         if graphtype == 'bar':
-            cursor.execute('''
-                 SELECT u.nome, p.nome, v.qtd, v.fat
-                 FROM vendas v
-                 JOIN auth_user u ON v.id_v = u.id
-                 JOIN produtos p ON v.id_p = p.id
-                 ''')
-            rows = cursor.fetchall()
-            connection.close()
-            df = pd.DataFrame(rows, columns=['vendedor', 'produto', 'qtd', 'fat'])
-            eixo_x_col = request.POST.get('eixo_x') # <option name="eixo_x" value="id_v"> <option name="eixo_x" value="id_p" >
-            y = request.POST.get('eixo_y') # <option name="eixo_y" value="qtd"> <option name="eixo_y" value="fat">
+            eixo_x_col = request.POST.get('eixo_x')
+            y = request.POST.get('eixo_y')
             if eixo_x_col == "id_v":
-                x = 'vendedor'
-                colors = 'produto'
+                x = 'vendedor' 
+                colors = 'produto' 
             else:
                 x = 'produto'
                 colors = 'vendedor'
             fig = px.bar(df, x=x, y=y, color=colors)
-            fig = fig.to_html(full_html=False)
         elif graphtype == 'pie':
-            cursor.execute('''
-                SELECT u.nome, p.nome, v.qtd, v.fat
-                FROM vendas v
-                JOIN auth_user u ON v.id_v = u.id
-                JOIN produtos p ON v.id_p = p.id
-                ''')
-            rows = cursor.fetchall()
-            connection.close()
-            df = pd.DataFrame(rows, columns=['vendedor', 'produto', 'qtd', 'fat'])
-            names = request.POST.get('names') # vendedor ou produto
-            values = request.POST.get('values') # quantidade ou faturamento
+            names = request.POST.get('names')
+            values = request.POST.get('values')
             fig = px.pie(df, names=names, values=values)
+        return JsonResponse({'fig': fig.to_html(full_html=False)})
+        
+@login_required
+def realizar_venda(request):
+    if request.method == "POST":
+        produto_id = request.POST.get("selection")
+        quantidade = int(request.POST.get("qtd"))
+        produto = get_object_or_404(Produto, id=produto_id)
+        if produto.estoque >= quantidade:
+            Venda.objects.create(
+                produto=produto,
+                vendedor=request.user,
+                qtd=quantidade,
+                fat=produto.valor*quantidade
+            )
+            produto.estoque -= quantidade
+            produto.save()
+            return JsonResponse({'mensagem': 'Venda realizada com sucesso!'})
+        else:
+            return JsonResponse({'mensagem': 'Estoque insuficiente.'})
+    produtos = Produto.objects.all()
+    vendas = Venda.objects.filter(vendedor=request.user)
+    return render(request, 'vendedor.html', {'Produtos': produtos, 'vendas': vendas})
+
+@login_required
+def editar_venda(request, venda_id):
+    venda = get_object_or_404(Venda, id=venda_id)
+    if venda.vendedor != request.user:
+        return redirect('paginaInicial')
+    if request.method == "POST":
+        quantidade = int(request.POST.get("quantidade"))
+        venda.quantidade = quantidade
+        venda.save()
+        return redirect('paginaInicial')
+    return render(request, "editar_venda.html", {"venda": venda})
+
+@login_required
+def listar_vendas(request):
+    produtos = Produto.objects.all()
+    vendas = Venda.objects.filter(vendedor=request.user)
+    return render(request, "vendedor.html", {
+        "produtos": produtos,
+        "vendas": vendas
+        })
+
+def adicionar_produto(request):
+    if request.method == "POST":
+        dados = json.loads(request.body)
+        nome = dados.get('nome')
+        valor = float(dados.get('valor'))
+        estoque = int(dados.get('estoque'))
+
+        Produto.objects.create(nome=nome, valor=valor, estoque=estoque)
+        return JsonResponse({'sucesso': True})
