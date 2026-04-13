7f11c628c76d0e3a784e49fe752664deda206d5a
David-Mandel0707 <dvdmndel@gmail.com>
2026-04-13 20:42:33 -0300

    Delete projeto_solar_salesmen directory

--- projeto_solar_salesmen/app_solar_salesmen/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# admin.py
-from .models import Produto, Venda
-
-admin.site.register(Produto)
-admin.site.register(Venda)
--- projeto_solar_salesmen/app_solar_salesmen/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppSolarSalesmenConfig(AppConfig):
-    name = 'app_solar_salesmen'
